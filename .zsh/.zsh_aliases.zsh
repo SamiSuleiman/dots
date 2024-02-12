988816911afde20cccf5b15cc749e7ff9ba1f240
sami <sulsami68@gmail.com>
2024-02-12 14:07:44 +0100

    aliases

--- .zsh/.zsh_aliases.zsh
+++ .zsh/.zsh_aliases.zsh
@@ -1,15 +1,24 @@
 # ls aliases
 alias ll='ls -alF'
-alias la='ls -A'
-alias l='ls -CF'
-alias ls='ls -l'
 alias c="clear"
 alias h="history -10" # last 10 history commands
 alias hc="history -c" # clear history
-alias ls="ls --color -lh"
+alias cp="cp -iv"
+alias mv="mv -iv"
+alias rm="rm -vI"
+alias bc="bc -ql"
+alias mkd="mkdir -pv"
+alias ls="exa --icons --group-directories-first"
+alias cat='bat'
+alias grep='grep --color=always'
 
-# aaaand no more ls???
 alias lll='nnn -deH'
+alias hhh='export HISTTIMEFORMAT="%F %T "; history | fzf | cut -d" " -f2- | bash'
+alias ccc='cd "$(dirname "$(find . -type f | fzf)")"'
+alias vif='nvim $(fzf)'
+alias pif='kill $(ps -ef | fzf | awk '\''{print $2}'\'')'
+alias gbf='git checkout $(git branch | fzf)'
+alias cdf='cd $(find * -type d | fzf)'
 
 # more aliases
 alias code='codium --profile=void-city'
