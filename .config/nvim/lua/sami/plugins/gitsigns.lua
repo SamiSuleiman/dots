7c4fe4614692b110ce74a9e5e5e31ba9e7f06b8a
sami <sulsami68@gmail.com>
2024-02-13 21:49:58 +0100

    nvim: show blame by default

--- .config/nvim/lua/sami/plugins/gitsigns.lua
+++ .config/nvim/lua/sami/plugins/gitsigns.lua
@@ -3,7 +3,7 @@ return {
         -- Adds git related signs to the gutter, as well as utilities for managing changes
         'lewis6991/gitsigns.nvim',
         opts = {
-            -- See `:help gitsigns.txt`
+            current_line_blame = true,
             signs = {
                 add = { text = '+' },
                 change = { text = '~' },
