f01ebe43ddfd4a1bfa8c6c9aa32b6ce89f1e3d3a
sami <sulsami68@gmail.com>
2024-02-13 20:53:14 +0100

    added neogit

--- .config/nvim/lua/sami/plugins/basic.lua
+++ .config/nvim/lua/sami/plugins/basic.lua
@@ -169,4 +169,17 @@ return {
     'tpope/vim-commentary',
     'windwp/nvim-ts-autotag',
     'stevearc/oil.nvim',
+
+    {
+        "NeogitOrg/neogit",
+        dependencies = {
+            "nvim-lua/plenary.nvim", -- required
+            "sindrets/diffview.nvim", -- optional - Diff integration
+
+            -- Only one of these is needed, not both.
+            "nvim-telescope/telescope.nvim", -- optional
+            "ibhagwan/fzf-lua",      -- optional
+        },
+        config = true
+    }
 }
