7fd5ca983940e4dd9d4ead7f0af9c9b0c28b0212
sami <sulsami@protonmail.com>
2024-02-24 22:40:19 +0100

    nvim oil configs

--- .config/nvim/lua/sami/configs/oil.lua
+++ .config/nvim/lua/sami/configs/oil.lua
@@ -1,4 +1,36 @@
 require("oil").setup({
+    default_file_explorer = true,
+    columns = {
+        "icon",
+        -- "permissions",
+        -- "size",
+        -- "mtime",
+    },
+    experimental_watch_for_changes = true,
+    delete_to_trash = true,
+    keymaps_help = {
+        border = "rounded",
+    },
+    -- Set to false to disable all of the above keymaps
+    use_default_keymaps = true,
+    view_options = {
+        -- Show files and directories that start with "."
+        show_hidden = true,
+        -- This function defines what is considered a "hidden" file
+        is_hidden_file = function(name, bufnr)
+            return vim.startswith(name, ".")
+        end,
+        -- This function defines what will never be shown, even when `show_hidden` is set
+        is_always_hidden = function(name, bufnr)
+            return false
+        end,
+        -- sort = {
+        --     -- sort order can be "asc" or "desc"
+        --     -- see :help oil-columns to see which columns are sortable
+        --     { "type", "asc" },
+        --     { "name", "asc" },
+        -- },
+    },
     keymaps = {
         ["g?"] = "actions.show_help",
         ["<CR>"] = "actions.select",
