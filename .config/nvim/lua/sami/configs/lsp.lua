65c0357fa6d915f4dc6c22c61edb8125b37c2a3d
sami <sulsami@protonmail.com>
2024-02-27 13:37:37 +0100

    nvim: remove redundant format command

--- .config/nvim/lua/sami/configs/lsp.lua
+++ .config/nvim/lua/sami/configs/lsp.lua
@@ -38,11 +38,6 @@ local on_attach = function(_, bufnr)
     nmap('<leader>wl', function()
         print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
     end, '[W]orkspace [L]ist Folders')
-
-    -- Create a command `:Format` local to the LSP buffer
-    vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-        vim.lsp.buf.format()
-    end, { desc = 'Format current buffer with LSP' })
 end
 
 -- document existing key chains
@@ -169,6 +164,7 @@ vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
     }
 )
 
+-- todo: replace this with the vim.api one
 vim.cmd([[
     augroup prettier
         autocmd!
@@ -176,6 +172,7 @@ vim.cmd([[
     augroup END
 ]])
 
+-- todo: add this to the `setup_handlers` function as an override
 local nvim_lsp = require('lspconfig')
 nvim_lsp.angularls.setup {
     on_attach = on_attach,
