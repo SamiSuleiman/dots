bfd38e1202727e1bace8e9f59e7d2ed62e5b4e9d
sami <sulsami@protonmail.com>
2024-03-01 22:04:11 +0100

    nvim: theme configs

--- .config/nvim/lua/sami/plugins/basic.lua
+++ .config/nvim/lua/sami/plugins/basic.lua
@@ -21,6 +21,26 @@ return {
             "Tsuzat/NeoSolarized.nvim",
             lazy = false,
             priority = 1000,
+            opts = {
+                style = "dark",         -- "dark" or "light"
+                transparent = true,     -- true/false; Enable this to disable setting the background color
+                terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+                enable_italics = true,  -- Italics for different hightlight groups (eg. Statement, Condition, Comment, Include, etc.)
+                styles = {
+                    -- Style to be applied to different syntax groups
+                    comments = { italic = true },
+                    keywords = { italic = true },
+                    functions = { bold = false },
+                    variables = {},
+                    string = { italic = true },
+                    underline = true, -- true/false; for global underline
+                    undercurl = true, -- true/false; for global undercurl
+                },
+                -- Add specific hightlight groups
+                on_highlights = function(highlights, colors)
+                    -- highlights.Include.fg = colors.red -- Using `red` foreground for Includes
+                end,
+            }
 
         },
         {
