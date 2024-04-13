16e0db900dc65bc9cab0d3559abfcdc8dad1e03f
sami <sulsami@protonmail.com>
2024-04-13 12:49:22 +0200

    gruv done

--- .config/nvim/lua/sami/plugins/based.lua
+++ .config/nvim/lua/sami/plugins/based.lua
@@ -304,6 +304,7 @@ return {
     'windwp/nvim-autopairs',
     event = 'InsertEnter',
     opts = {},
+    config = function() end,
   },
   { 'folke/which-key.nvim', opts = {} },
   {
@@ -713,30 +714,35 @@ return {
     priority = 1000,
     config = true,
     opts = {
-      {
-        terminal_colors = true, -- add neovim terminal colors
-        undercurl = true,
-        underline = true,
-        bold = true,
-        italic = {
-          strings = true,
-          emphasis = true,
-          comments = true,
-          operators = false,
-          folds = true,
-        },
-        strikethrough = true,
-        invert_selection = false,
-        invert_signs = false,
-        invert_tabline = false,
-        invert_intend_guides = false,
-        inverse = true, -- invert background for search, diffs, statuslines and errors
-        contrast = '', -- can be "hard", "soft" or empty string
-        palette_overrides = {},
-        overrides = {},
-        dim_inactive = false,
-        transparent_mode = false,
+      terminal_colors = true, -- add neovim terminal colors
+      undercurl = true,
+      underline = true,
+      bold = false,
+      italic = {
+        strings = true,
+        emphasis = true,
+        comments = true,
+        operators = false,
+        folds = true,
       },
+      strikethrough = true,
+      invert_selection = false,
+      invert_signs = false,
+      invert_tabline = false,
+      invert_intend_guides = false,
+      inverse = true, -- invert background for search, diffs, statuslines and errors
+      contrast = 'hard', -- can be "hard", "soft" or empty string
+      palette_overrides = {},
+      overrides = {
+        SignColumn = { bg = '#282828' },
+        ['@punctuation.bracket'] = { fg = '#E4D4AD' },
+        ['@punctuation.special'] = { fg = '#E4D4AD' },
+        ['@punctuation'] = { fg = '#E4D4AD' },
+        ['@symbol'] = { fg = '#E4D4AD' },
+        ['@operator'] = { fg = '#E4D4AD' },
+      },
+      dim_inactive = false,
+      transparent_mode = false,
     },
   },
   {
