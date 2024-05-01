77f5f04aaa32af70122609bff0a8c5c28eff84b7
sami <sulsami@protonmail.com>
2024-05-01 11:44:40 +0200

    nvim: add alternate file keymap

--- .config/nvim/lua/sami/keymaps/misc.lua
+++ .config/nvim/lua/sami/keymaps/misc.lua
@@ -3,3 +3,4 @@ vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = tr
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-u>', '<C-u>zz', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-d>', '<C-d>zz', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-x>', '<C-6>', { noremap = true })
