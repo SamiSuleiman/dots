d5534a82dcf877812b7779d15bf07cf660e44523
sami <sulsami@protonmail.com>
2024-07-04 11:21:10 +0200

    fish vi cursor

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -33,6 +33,12 @@ set -gx LSCOLORS ExGxBxDxCxEgEdxbxgxcxd
 
 set -g fish_key_bindings fish_vi_key_bindings
 bind -M insert \cc kill-whole-line repaint
+set fish_cursor_default block
+set fish_cursor_insert line
+set fish_cursor_replace_one underscore
+set fish_cursor_replace underscore
+set fish_cursor_external line
+set fish_cursor_visual block
 
 set -gx GOPATH $XDG_DATA_HOME/go
 
