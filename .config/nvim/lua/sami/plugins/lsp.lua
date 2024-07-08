acfbc1366c61793e4c219e1616ded80a804d96e1
sami <sulsami@protonmail.com>
2024-07-08 11:25:13 +0200

    disabled whichkey in neovim

--- .config/nvim/lua/sami/plugins/lsp.lua
+++ .config/nvim/lua/sami/plugins/lsp.lua
@@ -114,22 +114,23 @@ return {
         end, '[W]orkspace [L]ist Folders')
       end
 
-      -- document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-      }
-
-      require('which-key').register({
-        ['<leader>'] = { name = 'VISUAL <leader>' },
-        ['<leader>h'] = { 'Git [H]unk' },
-      }, { mode = 'v' })
+      if package.loaded['which-key'] then
+        local which_key = require 'which-key'
+        which_key.register {
+          ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
+          ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
+          ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
+          ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+          ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+          ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+          ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
+          ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+        }
+        which_key.register({
+          ['<leader>'] = { name = 'VISUAL <leader>' },
+          ['<leader>h'] = { 'Git [H]unk' },
+        }, { mode = 'v' })
+      end
 
       require('mason').setup()
       require('mason-lspconfig').setup()
