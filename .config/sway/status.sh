cb6087556336bca5a5a46e3fb251e28c0b5dd193
sami <sulsami@protonmail.com>
2024-07-20 11:00:12 +0200

    updated sway-bar script

--- .config/sway/status.sh
+++ .config/sway/status.sh
@@ -1,39 +1,60 @@
 #!/bin/bash
 
-# Date
-date_formatted=$(date "+%a %F %H:%M")
+function get_battery_status() {
+    bat_is_charging=$(cat /sys/class/power_supply/BAT0/status)
+    bat_percentage=$(cat /sys/class/power_supply/BAT0/capacity)
+    if [ $bat_percentage -ge 90 ]; then
+        battery_status="   ${bat_percentage}%"
+    elif [ $bat_percentage -ge 70 ]; then
+        battery_status=="   ${bat_percentage}%"
+    elif [ $bat_percentage -ge 50 ]; then
+        battery_status="   ${bat_percentage}%"
+    elif [ $bat_percentage -ge 30 ]; then
+        battery_status="   ${bat_percentage}%"
+    else
+        battery_status="   ${bat_percentage}%"
+    fi
+    if [ $bat_is_charging == "Charging" ]; then
+        battery_status=" ${bat_percentage}%"
+    fi
+    echo $battery_status
+}
 
-# Battery
-bat_is_charging=$(cat /sys/class/power_supply/BAT0/status)
-bat_percentage=$(cat /sys/class/power_supply/BAT0/capacity)
-if [ $bat_percentage -ge 90 ]; then
-    battery_status="   ${bat_percentage}%"
-elif [ $bat_percentage -ge 70 ]; then
-    battery_status=="   ${bat_percentage}%"
-elif [ $bat_percentage -ge 50 ]; then
-    battery_status="   ${bat_percentage}%"
-elif [ $bat_percentage -ge 30 ]; then
-    battery_status="   ${bat_percentage}%"
-else
-    battery_status="   ${bat_percentage}%"
-fi
-if [ $bat_is_charging == "Charging" ]; then
-   battery_status=" ${bat_percentage}%" 
-fi
+function get_network_status() {
+    wlan=$(iwgetid -r)
+    if [ -z $wlan ]; then
+        echo "   Not connected"
+    else
+        echo "   $wlan"
+    fi
+}
 
-# Volume
-volume=$(pamixer --get-volume-human)
-if [ $volume == "muted" ]; then
-    volume="   $volume"
-else
-    volume="   $volume"
-fi
+function get_volume_status() {
+    volume=$(pamixer --get-volume-human)
+    if [ $volume == "muted" ]; then
+        volume="   $volume"
+    else
+        volume="   $volume"
+    fi
+    echo $volume
+}
 
+function get_brightness_status() {
+    brightness=$(brightnessctl -m | grep -oP '\d+(?=%)' | head -1)
+    brightness="   $brightness%"
+    echo $brightness
+}
 
-# Brightness
-brightness=$(brightnessctl -m | grep -oP '\d+(?=%)' | head -1)
+function get_date() {
+    date=$(date "+%a %F | %H:%M")
+    date="   $date"
+    echo $date
+}
 
-# WLAN
-wlan=$(iwgetid -r)
+date_formatted=$(get_date)
+battery_status=$(get_battery_status)
+volume=$(get_volume_status)
+brightness=$(get_brightness_status)
+wlan=$(get_network_status)
 
-echo "  "$wlan"  |  "$volume"  |  "  $brightness"  |  "$battery_status"  |  " $date_formatted
+echo ""$wlan"  |  "$volume"  |  "$brightness"  |  "$battery_status"  |  "$date_formatted
