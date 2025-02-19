399816c884ca3e602e842565778eb38c4dc82f8e
AntennaTower <sulsami@protonmail.com>
2025-02-19 18:34:49 +0100

    stuff

--- .config/nvim/lua/sammy/commands/init.lua
+++ .config/nvim/lua/sammy/commands/init.lua
@@ -1,1 +1,2 @@
 vim.api.nvim_create_user_command('Bda', 'bufdo bd', {})
+vim.api.nvim_create_user_command('WW', 'bufdo w ++p', {})
--- .config/nvim/lua/sammy/keymaps/noice.lua
+++
@@ -1,1 +0,0 @@
-vim.api.nvim_set_keymap('n', '<leader>nd', ':NoiceDismiss<cr>', { noremap = true })
