a2db365fd68f54fd97f869227847e30c13d690f9
AntennaTower <sulsami@protonmail.com>
2025-02-23 12:56:52 +0100

    damn

--- .config/nvim/lua/antennatower/settings.lua
+++ .config/nvim/lua/antennatower/settings.lua
@@ -87,6 +87,7 @@ vim.treesitter.language.register('json', '.all-contributorsrc')
 vim.g.copilot_assume_mapped = true
 vim.opt.splitbelow = true
 vim.opt.splitright = true
+vim.opt.cedit = '<Esc>'
 
 vim.g.netrw_winsize = 20
 vim.g.netrw_banner = 0
