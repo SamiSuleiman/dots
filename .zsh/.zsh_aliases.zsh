9dc1d66b374286b01a9b2790ec326748d407b024
AntennaTower <antennatower@proton.me>
2025-03-05 15:50:13 +0100

    yazi

--- .zsh/.zsh_aliases.zsh
+++ .zsh/.zsh_aliases.zsh
@@ -38,3 +38,12 @@ fhist(){
   local chosen_command=$(cat ~/.zsh/.zsh_history | cut -d';' -f 2 | fzf --tac)
   eval $chosen_command
 }
+
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		builtin cd -- "$cwd"
+	fi
+	rm -f -- "$tmp"
+}
