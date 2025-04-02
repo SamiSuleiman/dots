86b7725121f99cf4bee8a98eeb114811b1c01dde
AntennaTower <antennatower@proton.me>
2025-04-02 21:16:58 +0200

    mental illness

--- .config/nvim/lua/antennatower/plugins/color.lua
+++ .config/nvim/lua/antennatower/plugins/color.lua
@@ -2,7 +2,7 @@ return {
   {
     'rose-pine/neovim',
     name = 'rose-pine',
-    enabled = true,
+    enabled = false,
     config = function()
       require('rose-pine').setup {
         dim_inactive_windows = false,
@@ -18,6 +18,8 @@ return {
             surface = 'NONE',
           },
           moon = {
+            -- base = '#0d0d0d',
+            -- surface = '#0d0d0d',
             base = 'NONE',
             surface = 'NONE',
           },
@@ -48,7 +50,69 @@ return {
           -- TelescopePromptBorder = { fg = 'surface', bg = '#0d0d0d' },
         },
       }
-      vim.cmd 'colorscheme rose-pine-moon'
+      vim.cmd 'colorscheme rose-pine'
+    end,
+  },
+  {
+    'ellisonleao/gruvbox.nvim',
+    priority = 1000,
+    enabled = true,
+    config = function()
+      require('gruvbox').setup {
+        terminal_colors = true, -- add neovim terminal colors
+        undercurl = true,
+        underline = true,
+        bold = true,
+        italic = {
+          strings = true,
+          emphasis = true,
+          comments = true,
+          operators = true,
+          folds = true,
+        },
+        strikethrough = true,
+        invert_selection = false,
+        invert_signs = false,
+        invert_tabline = false,
+        invert_intend_guides = false,
+        inverse = true, -- invert background for search, diffs, statuslines and errors
+        contrast = 'hard', -- can be "hard", "soft" or empty string
+        palette_overrides = {
+          -- bright_green = '#990000',
+        },
+        overrides = {
+          -- SignColumn = { bg = '#282828' },
+          -- ['@punctuation.bracket'] = { fg = '#E4D4AD' },
+          -- ['@punctuation.special'] = { fg = '#E5D4AD' },
+          -- ['@punctuation'] = { fg = '#E4D4AD' },
+          -- ['@symbol'] = { fg = '#E4D4AD' },
+          -- ['@operator'] = { fg = '#E5D4AD' },
+        },
+        dim_inactive = false,
+        transparent_mode = true,
+      }
+      vim.cmd 'colorscheme gruvbox'
+      local highlight = vim.api.nvim_set_hl
+
+      --change statlus line background color
+      highlight(0, 'StatusLine', { bg = '#111111' })
+      highlight(0, 'StatusLineNC', { bg = '#111111' })
+
+      -- change all background to black
+      -- vim.api.nvim_set_hl(0, 'Normal', { bg = 'NONE' })
+      -- vim.api.nvim_set_hl(0, 'SignColumn', { bg = 'NONE' })
+      -- vim.api.nvim_set_hl(0, 'VertSplit', { bg = 'NONE' })
+      -- vim.api.nvim_set_hl(0, 'StatusLine', { bg = 'NONE' })
+      -- vim.api.nvim_set_hl(0, 'StatusLineNC', { bg = 'NONE'})
+
+      -- vim.api.nvim_set_hl(0, '@variable', { bg = 'NONE' })
+      -- vim.api.nvim_set_hl(0, '@property', { bg = 'NONE' })
+      -- vim.api.nvim_set_hl(0, '@parameter', { bg = 'NONE' })
+      -- vim.api.nvim_set_hl(0, '@variable.parameter', { bg = 'NONE' })
+      -- vim.api.nvim_set_hl(0, 'Comment', { bg = 'NONE', fg = '#6c6c6c' })
+      -- vim.api.nvim_set_hl(0, 'CopilotSuggestion', { bg = 'NONE' })
+      -- vim.api.nvim_set_hl(0, 'String', { bg = 'NONE' })
+      -- vim.cmd 'highlight clear SignColumn'
     end,
   },
 }
